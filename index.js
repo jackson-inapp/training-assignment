3bfba508824fb6b261a2f47e257c3163524beaa0
jackson john <jackson.john@inapp.com>
2019-12-19 09:44:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`
+                    <h4>Hi, how are you</h4>
+                    <img src="https://www.incimages.com/uploaded_files/image/970x450/getty_503667408_2000133320009280259_352507.jpg"/>
+                `);
+        res.end();
+    }else{
+        res.write('Invalid URL ');
+        res.end();
+    }
+}).listen(3005);
